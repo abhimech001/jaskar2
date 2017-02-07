96ad54786a14fa32be2739f1a9a1badd7bad5aed
abhimech001 <abhi.mech001@gmail.com>
2017-02-07 17:12:14 +0530

    Update multiplysamplescript.spec.js

--- multiply/test/multiplysamplescript.spec.js
+++ multiply/test/multiplysamplescript.spec.js
@@ -4,11 +4,3 @@ describe('This is test suite',function() {
 		expect(a).toBe(10);
 	});	
 	});
-	
-	
-describe('This is test suite',function() {
-		it('This is test case with string',function() {
-		var b = multiplyNum(0,0);
-		expect(b).toBe(0);
-	});	
-	});
