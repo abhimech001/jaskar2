b89c0819531a7a0e6108a3a55c30e31d90250191
abhimech001 <abhi.mech001@gmail.com>
2017-02-07 17:31:43 +0530

    Update samplescript.spec.js

--- add/test/samplescript.spec.js
+++ add/test/samplescript.spec.js
@@ -7,6 +7,36 @@ describe('This is test suite',function() {
 	});
 	
 	});
+
+describe('This is test suite',function() {
+
+	it('This is test case with integer',function() {
+
+		var a = addNum(null,2);
+		expect(a).toBe(0);
+	});
+	
+	});
+	
+describe('This is test suite',function() {
+
+	it('This is test case with integer',function() {
+
+		var a = addNum(1,e);
+		expect(a).toBe(1);
+	});
+	
+	});
+	
+describe('This is test suite',function() {
+
+	it('This is test case with integer',function() {
+
+		var a = addNum(null,null);
+		expect(a).toBe(null);
+	});
+	
+	});
 	
 	
 //describe('This is test suite',function() {
