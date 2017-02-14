13f32bea3eecbb4b92f59a2183ba04cfbe30f2c8
abhimech001 <abhi.mech001@gmail.com>
2017-02-14 17:06:45 +0530

    updated grunt

--- gruntfile.js
+++ gruntfile.js
@@ -1,40 +1,43 @@
 module.exports = function(grunt) {
 
 	//var plugins = ['karma-jasmine']
-	var browsers = []
-        plugins.push('karma-chrome-launcher')
-        browsers.push('Chrome')
+	//var browsers = []
+        //plugins.push('karma-chrome-launcher')
+       // browsers.push('Chrome')
 
    grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
+		   pkg: grunt.file.readJSON('package.json'),
      // build: {
 	//expand: true,
 	//src: ['add/**/*.js','multiply/**/*.js'],
 	//dest: 'web/'
 	//},
-      jshint: {
+      //jshint: {
          // define the files to lint
-         all: ['Gruntfile.js', 'add/**/*.js','multiply/**/*.js'],
+         //all: ['Gruntfile.js', 'add/**/*.js','multiply/**/*.js'],
          // configure JSHint
-         options: { jshintrc: true
+         //options: { jshintrc: true
             // more options here if you want to override JSHint defaults
             //globals: {
                //jQuery: true,
-            }
+          //  }
          
-      },
+      
 
 	karma: {
 	 unit:{
       	   options: {
+		    {
+        configFile: 'karma-conf.js'
+      },
         frameworks: ['jasmine'],
-        singleRun: true,
-        browsers: ['PhantomJS'],
+		    unit{singleRun: true,},
+        //browsers: ['PhantomJS'],
         //plugins: plugins    
-        singleRun: true,
-        files: [
-          'jaskar2/**/*.js'
-        ]
+	//},
+       // files: [
+          //'jaskar2/**/*.js'
+       // ]
 		    }
 		    }
       },
@@ -68,15 +71,12 @@ module.exports = function(grunt) {
      // }
     //  }
   // });
-);
+		    });
   
-   grunt.loadNpmTasks('grunt-contrib-jshint');
+   //grunt.loadNpmTasks('grunt-contrib-jshint');
    //grunt.loadNpmTasks('grunt-contrib-watch');
    grunt.loadNpmTasks('grunt-karma');
-	   grunt.registerTask('test', [  
-  'jshint',
-  'karma'
-]);
+	   grunt.registerTask('default', ['karma']);
 
   // grunt.registerTask('default', ['jshint','build']);
    //grunt.registerTask('test', ['karma:single', 'karma:config'])
